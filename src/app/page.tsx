0d663e689bf9733d857a0a2ef93f8ceca0001fed
PankajKweera <pankajkweera4@gmail.com>
2025-09-16 18:49:23 +0530

    newfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import Header from '@/components/layout/Header'
 import Hero from '@/components/sections/Hero'
-import Footer from '@/components/layout/Footer'
-import ResponsiveTest from '@/components/ui/ResponsiveTest'
-import Container from '@/components/ui/Container'
+
+
 
 export default function Home() {
   return (
