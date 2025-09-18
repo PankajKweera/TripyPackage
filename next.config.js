7dab22dadb14391b8050f29534df759a82a5496a
PankajKweera <pankajkweera4@gmail.com>
2025-09-18 19:03:30 +0530

    Fix Netlify build by excluding sanity-studio from Next.js compilation

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
     unoptimized: true,
     domains: ['images.unsplash.com', 'via.placeholder.com'],
     formats: ['image/webp', 'image/avif'],
+  },
+  webpack: (config, { isServer }) => {
+    // Exclude sanity-studio from webpack compilation
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/sanity-studio/**', '**/sanity-schemas/**']
+    }
+    return config
   }
 }
 
